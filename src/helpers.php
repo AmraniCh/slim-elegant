46af0af6c2c1d5d45f73ebf8936c4f4c27d4f265
AmraniCh <contact@amranich.dev>
2022-11-09 18:10:47 +0100

    fix: fixed config helper function

--- src/helpers.php
+++ src/helpers.php
@@ -6,33 +6,23 @@ if (!function_exists('config')) {
     {
         global $app;
 
-        $container = $app->getContainer();
+        $settings = $app->getContainer()->get('settings');
 
-        if ($container->get('settings')->has($key)) {
-            $value = $container->get('settings')->get($key);
-        } else {
-            $path = dirname(__DIR__) . '/config/app.php';
-
-            if (!file_exists($path)) {
-                throw new \RuntimeException("'config/app.php' configuration file not exists.");
-            }
-
-            $config = include($path);
-
-            if (!array_key_exists($key, $config)) {
-                throw new \LogicException("'$key' not found in configuration variables file.");
-            }
-
-            $value = $config[$key];
+        if (!$settings->has($key)) {
+            throw new \LogicException("'$key' configuration variable not found.");
         }
+
+        $value = $settings->get($key);
 
         if ($value instanceof \Closure) {
             return call_user_func($value);
         }
-
+        
         if ($value === 'true') {
             return true;
-        } elseif ($value === 'false') {
+        }
+        
+        if ($value === 'false') {
             return false;
         }
 
