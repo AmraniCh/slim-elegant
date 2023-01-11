baef4d45d707fc0b58dd7a9ae8f760025b9c6dfa
AmraniCh <contact@amranich.dev>
2023-01-11 12:54:46 +0000

    reformed exception message

--- src/FileLoader/Exception/FileNotExistException.php
+++ src/FileLoader/Exception/FileNotExistException.php
@@ -7,7 +7,7 @@ class FileNotExistException extends FileLoaderException
 
     public function __construct(string $filePath, string $message = '', int $code = 0, ?\Throwable $previous = null)
     {
-        $message = "Failed to load the file with path '$filePath' because is not exist.";
+        $message = "File with path '$filePath' could not be loaded because it did not exist.";
         parent::__construct($message, $code, $previous);
     }
 }
